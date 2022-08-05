af943807c31a0aa798b1ca8c0d7d338d1413f242
nayakrujul <nayakrujul@hotmail.com>
2022-08-05 11:56:44 +0100

    Initial release

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+long_description = 'A file-based version of SQL for the command line.'
+
+setup(
+  name = 'badsql-tools',
+  version = '0.1',
+  license='Apache',
+  description = 'SQL but worse for the command line',
+  author = 'Rujul Nayak',
+  author_email = 'rujulnayak@outlook.com',
+  url = 'https://github.com/nayakrujul/badsql-tools',
+  download_url = 'https://github.com/nayakrujul/badsql-tools/archive/refs/tags/v_01.tar.gz',
+  keywords = ['database', 'SQL', 'file'],
+  install_requires=[
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha', 
+    'Intended Audience :: Developers',
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+  long_description=long_description,
+  long_description_content_type='text/x-rst',
+  packages = find_packages(),
+  entry_points ={
+            'console_scripts': [
+                'badsqldb = badsql-tools/db:db'
+            ]
+  }
+)
